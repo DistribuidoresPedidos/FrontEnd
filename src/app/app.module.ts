ef86079fdd2e1b4510f68937b6c3154d187e49b7
Felipe Guerrero <afguerreroa@unal.edu.co>
2017-03-23 19:58:43 -0500

    Added navbar and some assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavbarComponent } from './landing-page/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
