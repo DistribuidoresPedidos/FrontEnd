fc4c98b9db2b53265f0aa091565576cc42abfb65
jonatan <japarrat@unal.edu.co>
2017-03-27 23:09:35 -0500

    Creation validations

--- src/app/landing-page/content/contact/contact.component.ts
+++ src/app/landing-page/content/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserValidator } from './validations';
+
 
 @Component({
   selector: 'app-contact',
@@ -6,8 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  form: FormGroup;	  
 
-  constructor() { }
+  	constructor(private fb:FormBuilder){
+ 	this.form = fb.group({
+  		name: ['',  Validators.compose([
+  			Validators.required,
+  			])], 
+  		email: ['',  Validators.compose([
+  			Validators.required,
+  			])],
+  		number: ['',  Validators.compose([
+  			Validators.required
+  			])],
+  		message: ['',  Validators.compose([
+  			Validators.required,
+  			Validators.maxLength(250 ),
+  			])]
+
+  		})
+
+ 	}
 
   ngOnInit() {
   }
