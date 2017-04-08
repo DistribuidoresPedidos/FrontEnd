7a25f6302ce2a0a29197f9bbfaad84715ff84ac2
Jonas <joaortizro@unal.edu.co>
2017-04-08 17:38:58 -0500

    pipes for prices(staticBound)

---
+++ src/app/pipes/filter-offered-products-price.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterOfferedProductsPricePipe } from './filter-offered-products-price.pipe';
+
+describe('FilterOfferedProductsPricePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterOfferedProductsPricePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
